65db279f0622cc5f6e11620d83d21db3ee9bbc3a
Nikke-coder <niklas.isaksson@targetflow.fi>
2026-03-07 11:48:41 +0200

    Add files via upload

---
+++ cuuma-dashboard/src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://wzooguqwbuxepwkffwpp.supabase.co',
+  'sb_publishable_Tp7T9U_O6SDyG-066Oo5QA_DwdgTWQb'
+)
