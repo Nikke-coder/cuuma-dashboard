58331e2b27404af12e91ec2f7df3aeaafea42918
Nikke-coder <niklas.isaksson@targetflow.fi>
2026-03-09 15:04:04 +0200

    Delete cuuma-dashboard directory

--- cuuma-dashboard/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://wzooguqwbuxepwkffwpp.supabase.co',
-  'sb_publishable_Tp7T9U_O6SDyG-066Oo5QA_DwdgTWQb'
-)
